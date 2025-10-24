0a4cd38776d582977ad75f0c45ef83d987e6a610
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-10-24 01:55:33 +0100

    update CORS config in prod settings

--- nams_voting_platform/settings/base.py
+++ nams_voting_platform/settings/base.py
@@ -31,10 +31,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware", 
+    "corsheaders.middleware.CorsMiddleware",  
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
--- nams_voting_platform/settings/prod.py
+++ nams_voting_platform/settings/prod.py
@@ -6,16 +6,26 @@ DEBUG = False
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+else:
+    ALLOWED_HOSTS += ["localhost", "127.0.0.1"]
 
-# Security
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-
-# CORS & CSRF
 CORS_ALLOW_ALL_ORIGINS = False
+
 frontend_urls = os.getenv("FRONTEND_URLS", "")
 if frontend_urls:
     CORS_ALLOWED_ORIGINS = [url.strip().rstrip('/') for url in frontend_urls.split(",")]
+    CSRF_TRUSTED_ORIGINS = [url.strip().rstrip('/') for url in frontend_urls.split(",")]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
     CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
+
+CORS_ALLOW_CREDENTIALS = True
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
